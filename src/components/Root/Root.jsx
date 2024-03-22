b35323e5b086348ee31235a6ff93aff743f17796
Shawon301910 <shawonkumarsarkar.bd@gmail.com>
2024-03-22 23:45:09 +0600

    nav and footer complete

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+
+
+const Root = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Root;
